b31de2660a70e24dede2c8c328633f53d2005a85
SamPunch <57600664+SamPunch@users.noreply.github.com>
2019-11-11 13:14:06 +0000

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -25,13 +25,17 @@ $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: test@noreply.google.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
+if(mail($to,$email_subject,$email_body,$headers)){
+	ChromePhp::log('Working tfuck');}
+else{
+	ChromePhp::log('Were fucked boys'
+}
 	ChromePhp::log($to);
 	ChromePhp::log($email_subject);
 	ChromePhp::log($email_body);
 	ChromePhp::log($headers);
 
-	ChromePhp::log('Working tfuck');
+	
 return true; 
 }        
 ?>
